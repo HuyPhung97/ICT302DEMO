db5304f0aa720bd05294466107d4870b57d66498
HuyPhung97 <tuthaiphu1@gmai.com>
2020-07-02 18:14:15 +0800

    first commit

--- handleEvent/studentEvent.js
+++ handleEvent/studentEvent.js
@@ -99,7 +99,7 @@ function createURLStudent()
                             }
                             else 
                             {
-                                res.render(path.join(__dirname , "../htmlPage/student/StudentSite.html"),
+                                res.sendFile(path.join(__dirname , "../htmlPage/student/StudentSite.html"),
                                 {
                                     PersonId : data[i].PersonId,
                                     SurName : data[i].Surname,
